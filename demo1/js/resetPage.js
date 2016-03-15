dbf32451e916524381319bc136e40c1440be2e62
sky <skyshuilan@163.com>
2016-03-15 09:34:33 +0800

    add demo1 about mobile

---
+++ demo1/js/resetPage.js
@@ -0,0 +1,12 @@
+function resetPage() {
+
+	var deviceWidth = document.documentElement.clientWidth,
+	scale = deviceWidth/750;
+	document.body.style.zoom = scale;
+}
+window.onload = function(){
+	resetPage();
+}
+window.onresize = function(){
+	resetPage();
+}
